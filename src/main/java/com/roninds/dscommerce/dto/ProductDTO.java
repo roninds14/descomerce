bb2f7eb9a5202e18bc65a77701dc88416698c736
roninds14 <roninds14@gmail.com>
2023-12-15 21:10:55 -0300

    Fix validation inert product

--- src/main/java/com/roninds/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/roninds/dscommerce/dto/ProductDTO.java
@@ -4,11 +4,11 @@ import com.roninds.dscommerce.entities.Category;
 import com.roninds.dscommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +28,7 @@ public class ProductDTO {
     private String description;
 
     @Positive(message = "O preço deve ser positivo")
+    @NotNull(message = "Campo requerido")
     private Double price;
 
     @Size(max = 255, message = "Não pode conter mais de 255 caracteres")
@@ -36,7 +37,7 @@ public class ProductDTO {
     @NotEmpty(message = "Deve conter ao menos uma categoria")
     private List<CategoryDTO> categories = new ArrayList<>();
 
-    public ProductDTO(@NotNull Product product) {
+    public ProductDTO(@org.jetbrains.annotations.NotNull Product product) {
         this.id = product.getId();
         this.name = product.getName();
         this.description = product.getDescription();
